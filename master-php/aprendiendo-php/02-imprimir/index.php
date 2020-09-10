3c0e4d5f8ee0a7e42741c28c23fc1ac1e37eb8a9
MiGueAJM9724 <miguejm@outlook.es>
2020-09-09 20:56:10 -0500

    Imprimir

---
+++ master-php/aprendiendo-php/02-imprimir/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta/>
+        <title>Imprimir por pantalla -.Master en PHP</title>
+    </head>
+    <body>
+        <h1>Master en PHP - victorroblesweb.es</h1>
+        <?php
+        echo "<h3>Listado de videojuegos:</h3>";
+        echo "<ul>"
+            . "<li>GTA</li>"
+            . "<li>Fifa</li>"
+            . "<li>Mario Bros</li>"
+            . "</ul>";
+
+        echo '<p>Esta es toda'. ' - '.'lista de juegos </p>';
+        ?>
+    </body>
+</html>
