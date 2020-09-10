1ab998a66325002444210aa5234d409a9d1f3a84
MiGueAJM9724 <miguejm@outlook.es>
2020-09-10 09:42:40 -0500

    Variables

---
+++ master-php/aprendiendo-php/03-variables/index.php
@@ -0,0 +1,9 @@
+<?php
+//  Una variable es un contenedor de información, puedo guardar cualquier dato de ella
+$mi_primera_variable = "Hola mundo desde una variable";
+$numero = 24;
+$verdadero = true;
+
+echo '<h1>'.$mi_primera_variable.'</h1>';
+echo $numero.'<br>';
+?>
