dac595d412bfa585396d04bc805b54cfeb43c398
MiGueAJM9724 <miguejm@outlook.es>
2020-09-11 09:18:53 -0500

    operadores aritmeticos

---
+++ master-php/aprendiendo-php/06-operadores/index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+    Operadores aritmeticos
+*/
+$numero1 = 55;
+$numero2 = 33;
+
+echo 'Suma: '.($numero1 + $numero2).'<br/>';
+echo 'Resta: '.($numero1 - $numero2).'<br/>';
+echo 'Multiplicación: '.($numero1 * $numero2).'<br/>';
+echo 'División: '.($numero1 / $numero2).'<br/>';
+echo 'Resto: '.($numero1 % $numero2).'<br/>';
+
+
