f811612dc4d794cccc30f73bcf4dfa0687084d26
MiGueAJM9724 <miguejm@outlook.es>
2020-09-11 09:31:28 -0500

    operadores de incremento y decremento

--- master-php/aprendiendo-php/06-operadores/index.php
+++ master-php/aprendiendo-php/06-operadores/index.php
@@ -11,4 +11,14 @@ echo 'Multiplicación: '.($numero1 * $numero2).'<br/>';
 echo 'División: '.($numero1 / $numero2).'<br/>';
 echo 'Resto: '.($numero1 % $numero2).'<br/>';
 
-
+//  Operadores de incremnto y decremento
+$year = 2020;
+//  incremento  $year = $year + 1;
+$year++;
+//  decremento $year = $year - 1;
+$year--;
+// pre-decremento   $year = 1 - $year;
+--$year;
+//pre-incremento $year = 1 + $year;
+echo "<h1>$year</h1>";
+?>
