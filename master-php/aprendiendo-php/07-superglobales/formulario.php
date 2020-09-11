b11375ef8a0a3b7139a1af5f7e1bb1580a5dbcc2
MiGueAJM9724 <miguejm@outlook.es>
2020-09-11 17:03:34 -0500

    Recibir datos por metodo POST

--- master-php/aprendiendo-php/07-superglobales/formulario.php
+++ master-php/aprendiendo-php/07-superglobales/formulario.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <h1>Formulario en PHP</h1>
-        <form method="GET" action="recibir.php">
+        <form method="POST" action="recibir.php">
             <p>
                 <label for="nombre">Nombre</label>
                 <input type="text" name="nombre" />
--- master-php/aprendiendo-php/07-superglobales/recibir.php
+++ master-php/aprendiendo-php/07-superglobales/recibir.php
@@ -1,5 +1,5 @@
 <?php
-echo '<h1>'.$_GET['nombre'].'</h1>';
-echo '<h1>'.$_GET['apellido'].'</h1>';
+echo '<h1>'.$_POST['nombre'].'</h1>';
+echo '<h1>'.$_POST['apellido'].'</h1>';
 
-var_dump($_GET);
+var_dump($_POST);
