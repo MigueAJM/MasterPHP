2fb04881af99452d962aa270c3ba39dd0bfa9a71
MiGueAJM9724 <miguejm@outlook.es>
2020-09-11 16:43:18 -0500

    variables globales

---
+++ master-php/aprendiendo-php/07-superglobales/index.php
@@ -0,0 +1,15 @@
+<?php
+//  variables super globales
+
+//  variables del servidor
+echo '<h1>'.$_SERVER['SERVER_ADDR'].'</h1>'; // ip
+
+echo '<h1>'.$_SERVER['SERVER_NAME'].'</h1>'; // dominio
+
+echo '<h1>'.$_SERVER['SERVER_SOFTWARE'].'</h1>'; // entorno de desarrollo
+
+echo '<h1>'.$_SERVER['SERVER_USER_AGENT'].'</h1>'; //
+
+echo '<h1>'.$_SERVER['SERVER_ADDR'].'</h1>';
+
+echo '<h1>'.$_SERVER['SRENOTE_ADDR'].'</h1>'; //ip cliente
