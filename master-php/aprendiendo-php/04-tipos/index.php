2fde8298dc45e76b93af3b701fdb2135b4de6183
MiGueAJM9724 <miguejm@outlook.es>
2020-09-11 08:48:28 -0500

    tipos de datos

---
+++ master-php/aprendiendo-php/04-tipos/index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+    TIPOS DE DATOS
+    Entero ( int/integer) = 99
+    Coma flotante / decimales (float/double) = 3.45
+    Cadenas (string) = "Hola yo soy un string"
+    Boleano (bool) =  true/false
+    null    (no lleva contenido dentro)
+    array   (coleccion de datos)
+    Objetos 
+*/
+$numero = 100;
+$decimal = 27.90;
+$tecto = "Soy texto";
+$verdadero = true;
+$nula;
+echo gettype($nula);
