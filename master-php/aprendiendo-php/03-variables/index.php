1e2e791311c41db390fb89897dfdea7d7d92930e
MiGueAJM9724 <miguejm@outlook.es>
2020-09-11 08:38:46 -0500

    Mas variables

--- master-php/aprendiendo-php/03-variables/index.php
+++ master-php/aprendiendo-php/03-variables/index.php
@@ -3,7 +3,9 @@
 $mi_primera_variable = "Hola mundo desde una variable";
 $numero = 24;
 $verdadero = true;
-
+$numero = 77;
 echo '<h1>'.$mi_primera_variable.'</h1>';
 echo $numero.'<br>';
+$numero = 120;
+echo $numero;
 ?>
