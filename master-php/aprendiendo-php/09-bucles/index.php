15229ba7d788e3dde186b95a36ab18955da21337
MiGueAJM9724 <miguejm@outlook.es>
2020-09-12 18:35:59 -0500

    Ejemplo 2 WHILE

--- master-php/aprendiendo-php/09-bucles/index.php
+++ master-php/aprendiendo-php/09-bucles/index.php
@@ -22,3 +22,25 @@ while($numero <= 100){
     if($numero != 100) echo ', ';
     $numero++;
 }
+
+//  Ejemplo
+echo '<hr>';
+if(isset($_GET['numero'])){ //  isset comprueba si existe una variable o no, o si existe un dato o no
+    //pasamos de string a int =>    casteo
+    $numero = (int)$_GET['numero']; //(int)$_GET['numero'] esto se conoce como casting de datos 
+}else{
+    $numero = 1;
+}
+/*
+*   NOTA:   cuando se reciben datos por la url
+*   son de tupo string
+*/
+var_dump($numero); 
+
+echo '<h1>Tabla de multiplicar del numero'.$numero.'</h1>';
+
+$contador = 0;
+while($contador <= 10){
+    echo "$numero x $contador = ".($numero*$contador).'<br>';
+    $contador++;
+}
