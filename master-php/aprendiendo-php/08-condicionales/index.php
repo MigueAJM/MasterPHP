4bec016e174b05244668fc98d00f7946253f30d0
MiGueAJM9724 <miguejm@outlook.es>
2020-09-11 21:12:05 -0500

    Ejemplo 3 Condicional IF

--- master-php/aprendiendo-php/08-condicionales/index.php
+++ master-php/aprendiendo-php/08-condicionales/index.php
@@ -18,7 +18,7 @@
  *  <= menor y igual que
  *  >= Mayor igual que
 */
-
+//  Ejemplo 1
 $color = "rojo";
 if($color == 'rojo'){
     echo 'El color es rojo';
@@ -27,10 +27,31 @@ if($color == 'rojo'){
 }
 
 echo '<br>';
-
+//Ejemplo 2
 $year = 2021;
 if($year != 2020){
     echo 'Es un año diferente a '.$year;
 }else{
-    echo 'Estamos en '.$year
+    echo 'Estamos en '.$year;
+}
+
+//  Ejemplo 3
+echo '<br>';
+
+$nombre = "Miguel";
+$ciudad = 'Mexico';
+$continete = 'America';
+$edad = 23;
+$mayoria_edad = 18;
+
+if($edad >= $mayoria_edad){
+    echo '<h1>'.$nombre.' es mayor de edad</h1>';
+
+    if($continete == 'America'){
+        echo '<h2>Es de '.$ciudad.'</h2>';
+    }else{
+        echo 'No es de '.$continete;
+    }
+}else{
+    echo '<h2>'.$nombre.' no es mayor de edad</h2>';
 }
