dfd72d48e642a49c2d9c1deec49f85c1b9f031f8
MiGueAJM9724 <miguejm@outlook.es>
2020-09-13 10:50:48 -0500

    Bloque Ejercicios Ej1

---
+++ master-php/aprendiendo-php/11-ejercicicos/ejercicio1.php
@@ -0,0 +1,10 @@
+<?php
+/*
+*   Ejercicio 1
+*   Crear 2 variables "pais" y "continente" y motrar su valor por pantalla(imprimir)
+*   Poner en un comentario que tipo de dato tiene
+*
+*/
+$pais = 'México';   //  String
+$continente = 'America';    //string
+echo '<h1>Pais: '.$pais.', continente: '.$continente.'</h1>';
