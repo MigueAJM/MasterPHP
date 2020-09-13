b122d7687c50d5316e5e9a83747d4b8d45e35bb5
MiGueAJM9724 <miguejm@outlook.es>
2020-09-13 14:43:56 -0500

    Bloque Ejercicios Ej7

---
+++ master-php/aprendiendo-php/11-ejercicicos/ejercicio7.php
@@ -0,0 +1,22 @@
+<?php
+/*
+*   Ejercicio 7.    Hacer un script que muestre todos los numeros impares entre dos numeros
+*   que nos lleguen por la URL($_GET)
+*
+*   NOTA => isset — Determine if a variable is declared and is different than NULL
+*/
+if(isset($_GET['numero1']) && isset($_GET['numero2'])){
+    $numero1 = $_GET['numero1'];
+    $numero2 = $_GET['numero2'];
+    if($numero1 < $numero2){
+        for($i = $numero1; $i <= $numero2; $i++){
+            if($i%2 != 0){
+                echo '<h4>'.$i.'</h4>';
+            }
+        }
+    }else{
+        echo 'El numero 1 debe ser menor al numero 2';
+    }
+}else{
+    echo '<h3>Los parametros get no eisten</h3>';
+}
