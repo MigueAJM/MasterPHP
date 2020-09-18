18017bb4a62ef1ac0b4174644fc9ead048fce0af
MiGueAJM9724 <miguejm@outlook.es>
2020-09-18 15:20:16 -0500

    Funciones predefinidas

---
+++ master-php/aprendiendo-php/12-funciones/predefinidas.php
@@ -0,0 +1,14 @@
+<?php
+//  Debugear
+$nombre = 'Miguel';
+var_dump($nombre);
+
+//  Fechas
+echo '<br>'.date('d-m-y').'<br>';
+echo time().'<hr>';
+
+//  Matematicas
+echo 'Raiz cuadrada de 10: '.sqrt(10).'<br>';
+echo 'Numero aleatorio entre 10 y 40: '.rand(10, 40).'<br>';
+echo 'Numero pi: '.pi().'<br>'; 
+echo 'Redondear: '.round(7.891234, 2);
