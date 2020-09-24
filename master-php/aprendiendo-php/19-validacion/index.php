b7e6ddd8b58a6bd22c0671b8cfdfb2aa1e109b4c
MiGueAJM9724 <miguejm@outlook.es>
2020-09-24 16:06:30 -0500

    validación con HTML

--- master-php/aprendiendo-php/19-validacion/index.php
+++ master-php/aprendiendo-php/19-validacion/index.php
@@ -32,19 +32,19 @@
     ?>
     <form action="procesar_formulario.php" method="POST">
         <label for="nombre">Nombre: </label><br/>
-        <input type="text" name="nombre" /><br/>
+        <input type="text" name="nombre" required="required" pattern="[a-zA-Z ]+"/><br/>
 
         <label for="apellidos">Apellidos: </label><br/>
-        <input type="text" name="apellidos" /><br/>
+        <input type="text" name="apellidos" required="required" pattern="[a-zA-Z ]+"/><br/>
 
         <label for="edad">Edad: </label><br/>
-        <input type="number" name="edad" /><br/>
-
+        <input type="number" name="edad" required="required" pattern="[0-9]+"/><br/>
+       
         <label for="email">Email: </label><br/>
-        <input type="text" name="email" /><br/>
+        <input type="text" name="email" required="required"/><br/>
 
         <label for="pass">Contraseña: </label><br/>
-        <input type="password" name="pass" /><br/>
+        <input type="password" name="pass" required="required" minlength="5"/><br/>
 
         <input type="submit" value="Enviar"/>
     </form>
