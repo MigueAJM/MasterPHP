7afad05e5afc5832d94f20f6538bc78b79740f56
MiGueAJM9724 <miguejm@outlook.es>
2020-09-26 19:39:45 -0500

    mostrar imagenes en la web

--- master-php/aprendiendo-php/22-subidas/index.php
+++ master-php/aprendiendo-php/22-subidas/index.php
@@ -11,5 +11,18 @@
         <input type="file" name="archivo"/>
         <input type="submit" value="Enviar"/>
     </form>
+    <h1>Listado de imagenes</h1>
+    <?php
+    $gestor = opendir('./images');
+    if($gestor):
+        while(($image = readdir($gestor)) !== false):
+            if($image != '.' && $image != '..'):
+                echo "<img src='images/$image' width='20px'/><br/>";
+            endif;
+        endwhile;
+    endif;
+    
+    ?>
+</body>
 </body>
 </html>
