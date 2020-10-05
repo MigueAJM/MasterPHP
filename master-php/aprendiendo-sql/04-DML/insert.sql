086728c8bf8a6840cb277544cbf5047353b8e143
MiGueAJM9724 <miguejm@outlook.es>
2020-10-05 12:52:56 -0500

    insert avanzados

--- master-php/aprendiendo-sql/04-DML/insert.sql
+++ master-php/aprendiendo-sql/04-DML/insert.sql
@@ -2,3 +2,7 @@
 INSERT INTO usuarios VALUES(null, 'Miguel', 'Jimenez', 'migue@migue.com', 'holamundo', '2020-10-05');
 INSERT INTO usuarios VALUES(null, 'Angel', 'Jimenez', 'angel@angel.com', 'holamundo', '2020-10-05');
 INSERT INTO usuarios VALUES(null, 'Sebastian', 'Jimenez', 'sebastian@sebastian.com', 'holamundo', '2020-10-05');
+
+--  INSERTAR FILAS CON CIERTAS COLUMNAS
+INSERT INTO  usuarios(email, password) VALUES('admin@admin.com', 'admin'); --   en este caso fallaria por las restricciones 
+
