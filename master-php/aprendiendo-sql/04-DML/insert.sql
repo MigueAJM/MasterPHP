9ab1e4d499b5c86960241ac4bdbf348945b8471e
MiGueAJM9724 <miguejm@outlook.es>
2020-10-05 10:55:50 -0500

    insert basico

---
+++ master-php/aprendiendo-sql/04-DML/insert.sql
@@ -0,0 +1,4 @@
+--  INSERTAR NUEVOS REGISTROS
+INSERT INTO usuarios VALUES(null, 'Miguel', 'Jimenez', 'migue@migue.com', 'holamundo', '2020-10-05');
+INSERT INTO usuarios VALUES(null, 'Angel', 'Jimenez', 'angel@angel.com', 'holamundo', '2020-10-05');
+INSERT INTO usuarios VALUES(null, 'Sebastian', 'Jimenez', 'sebastian@sebastian.com', 'holamundo', '2020-10-05');
