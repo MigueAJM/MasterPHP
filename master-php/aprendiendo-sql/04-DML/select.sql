35e1307bbe58aa61cab80207531b17b89dba9a1b
MiGueAJM9724 <miguejm@outlook.es>
2020-10-06 07:26:50 -0500

    funciones sobre texto

--- master-php/aprendiendo-sql/04-DML/select.sql
+++ master-php/aprendiendo-sql/04-DML/select.sql
@@ -18,3 +18,20 @@ SELECT id, email, (id+3) AS 'OPERACION' FROM usuarios ORDER BY id DESC;
 SELECT ABS(7) AS 'OPERACION' FROM usuarios;     -- valor absoluto
 SELECT CEIL(7.34) AS 'OPERACION' FROM usuarios;  -- redondear
 SELECT FLOOR(7.34) AS 'OPERACION' FROM usuarios;  -- redondear a la baja
+SELECT RAND() AS 'OPERACION' FROM usuarios;  -- numero aleatorio
+SELECT PI() AS 'OPERACION' FROM usuarios;  -- valor de PI
+SELECT ROUND(7.34, 1) AS 'OPERACION' FROM usuarios;  -- redondear permitiendo dejar los digitos necesarios
+SELECT TRUNCATE(7.34, 1) AS 'OPERACION' FROM usuarios;  -- permite quitar decimales
+
+#   https://www.w3resource.com/sql/sql-functions.php   -- Mas funciones math y de texto 
+
+--  FUNCIONES SOBRE TEXTOS
+SELECT nombre FROM usuarios;
+SELECT UPPER(nombre) FROM usuarios;     -- retorna mayusculas
+SELECT LOWER(nombre) FROM usuarios;     -- retorna minusculas
+SELECT CONCAT(nombre, ' ', apellidos) AS 'NOMBRE COMPLETO' FROM usuarios;   -- retorna una concatenación
+SELECT UPPER(CONCAT(nombre, ' ', apellidos)) AS 'CONVERSION' FROM usuarios; -- retorna mayusculas y la concatenación
+SELECT LENGTH(CONCAT(nombre, ' ', apellidos)) AS 'LONGITUD' FROM usuarios;  -- retorna la longitud
+SELECT TRIM(CONCAT('     ', nombre, ' ', apellidos)) AS 'ELIMINAR ESPACIOS' FROM usuarios; -- elimina los espacios de la derecha e izquierda de una cadena de texto
+
+
