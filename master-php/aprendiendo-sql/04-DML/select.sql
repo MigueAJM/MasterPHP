adb1777a51ddb5d57d79ebc433ff9df05d8773d3
MiGueAJM9724 <miguejm@outlook.es>
2020-10-06 13:48:33 -0500

    funciones sobre fechas

--- master-php/aprendiendo-sql/04-DML/select.sql
+++ master-php/aprendiendo-sql/04-DML/select.sql
@@ -34,4 +34,19 @@ SELECT UPPER(CONCAT(nombre, ' ', apellidos)) AS 'CONVERSION' FROM usuarios; -- r
 SELECT LENGTH(CONCAT(nombre, ' ', apellidos)) AS 'LONGITUD' FROM usuarios;  -- retorna la longitud
 SELECT TRIM(CONCAT('     ', nombre, ' ', apellidos)) AS 'ELIMINAR ESPACIOS' FROM usuarios; -- elimina los espacios de la derecha e izquierda de una cadena de texto
 
+--  FUNCIONES SOBRE FECHAS
+SELECT fecha FROM usuarios;
+SELECT email, fecha, CURDATE() AS 'NOW' FROM usuarios; -- FECHA ACTUAL
+SELECT email, DATEDIFF(CURDATE(), fecha) AS 'DIFERENCIA-DÍAS' FROM usuarios; -- DIFERENCIA DE DIAS 
+SELECT email, DAYNAME(fecha) AS 'DIA' FROM usuarios; -- NOMBRE DEL DÍA
+SELECT email, DAYOFMONTH(fecha) AS 'DIA/MES' FROM usuarios; -- DIA DL MES (numero)
+SELECT email, DAYOFWEEK(fecha) AS 'DIA/SEMANA' FROM usuarios; -- DIA DE LA SEMANA (numero)
+SELECT email, DAYOFYEAR(fecha) AS 'DIA/AÑO' FROM usuarios; -- DIA DEL AÑO (numero)
+SELECT email, MONTH(fecha) AS 'MES' FROM usuarios; -- RETORNA EL MES (numero)
+SELECT email, YEAR(fecha) AS 'AÑO' FROM usuarios; -- retorna el año
+SELECT email, DAY(fecha) AS 'DIA' FROM usuarios; -- DIA DE LA dia
+SELECT email, HOUR(fecha) AS 'HORA' FROM usuarios; -- RETORNA LA HORA
+SELECT CURTIME() AS 'HORA ACTUAL'; -- HORA ACTUAL
+SELECT SYSDATE() AS 'HORA SO'; -- HORA DEL SO
+SELECT DATE_FORMAT(fecha, '%d-%m-%Y') AS 'FECHA/FORMATO' FROM usuarios; -- DAR FORMATO A LAS FECHAS
 
