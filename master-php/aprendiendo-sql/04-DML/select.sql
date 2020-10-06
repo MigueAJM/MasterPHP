aff85805b3b665e43d2223347ad850d99c7af46d
MiGueAJM9724 <miguejm@outlook.es>
2020-10-06 07:13:32 -0500

    funciones matematicas

--- master-php/aprendiendo-sql/04-DML/select.sql
+++ master-php/aprendiendo-sql/04-DML/select.sql
@@ -13,3 +13,8 @@ SELECT email, (4/7) AS operacion FROM usuarios;
 SELECT id, email FROM usuarios ORDER BY id ASC;
 SELECT id, email FROM usuarios ORDER BY id DESC;
 SELECT id, email, (id+3) AS 'OPERACION' FROM usuarios ORDER BY id DESC;
+
+--  FUNCIONES MATEMATICAS
+SELECT ABS(7) AS 'OPERACION' FROM usuarios;     -- valor absoluto
+SELECT CEIL(7.34) AS 'OPERACION' FROM usuarios;  -- redondear
+SELECT FLOOR(7.34) AS 'OPERACION' FROM usuarios;  -- redondear a la baja
