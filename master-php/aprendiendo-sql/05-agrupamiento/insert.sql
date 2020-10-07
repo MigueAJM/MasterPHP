7122bd6ac178f364cb9975726d9de60dbf1c6889
MiGueAJM9724 <miguejm@outlook.es>
2020-10-07 15:07:31 -0500

    rellenando tablas

---
+++ master-php/aprendiendo-sql/05-agrupamiento/insert.sql
@@ -0,0 +1,18 @@
+-- INSERT PARA CATEGORIAS
+INSERT INTO categorias VALUES(null,'Acción');
+INSERT INTO categorias VALUES(null,'Rol');
+INSERT INTO categorias VALUES(null,'Deportes');
+
+
+-- INSERT PARA ENTRADAS
+INSERT INTO entradas VALUES(null, 1, 3, 'Novedades de FIFA20 Online', 'Review del FIFA20', CURDATE());
+INSERT INTO entradas VALUES(null, 1, 1, 'Novedades de GTA 5 Online', 'Todo sobre GTA 5', CURDATE());
+INSERT INTO entradas VALUES(null, 1, 2, 'Nuevso jugadores de FIFA20', 'Han llegado nuevos retos a este juego', CURDATE());
+
+INSERT INTO entradas VALUES(null, 2, 3, 'Novedades de FIFA21 Online', 'Review del FIFA21', CURDATE());
+INSERT INTO entradas VALUES(null, 2, 1, 'Novedades de Resident Evil Online', 'Todo sobre Resident Evil', CURDATE());
+INSERT INTO entradas VALUES(null, 2, 2, 'Nuevso jugadores de FIFA21', 'Han llegado nuevos retos a este juego', CURDATE());
+
+INSERT INTO entradas VALUES(null, 3, 3, 'Novedades de NBA21 Online', 'Review del NBA21', CURDATE());
+INSERT INTO entradas VALUES(null, 3, 1, 'Novedades de Crash Online', 'Todo sobre Crash', CURDATE());
+INSERT INTO entradas VALUES(null, 3, 2, 'Nuevso jugadores de ESPORT', 'Han llegado nuevos retos a este juego', CURDATE());
