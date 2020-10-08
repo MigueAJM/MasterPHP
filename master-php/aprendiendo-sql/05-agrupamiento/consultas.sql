47a3c0e995b38e41d227429cef7a8dc2374f7e8d
MiGueAJM9724 <miguejm@outlook.es>
2020-10-08 08:53:08 -0500

    condiciones HAVING

--- master-php/aprendiendo-sql/05-agrupamiento/consultas.sql
+++ master-php/aprendiendo-sql/05-agrupamiento/consultas.sql
@@ -1,2 +1,13 @@
 -- CONSULTAS DE AGRUPAMIENTO
-SELECT COUNT(titulo) AS 'NUMERO DE ENTRADAS', categoria_id from entradas GROUP BY categoria_id;
+SELECT COUNT(titulo) AS 'NUMERO DE ENTRADAS', categomeria_id from entradas GROUP BY categoria_id;
+/*
+NOTA: clausala WHERE actua sobre una columna, 
+no actu sobre un grupo de datos
+HAVING
+Esta clusala tiene la misma funcionalidad que la clusala WHERE, solo que esta vaenfocada
+a agrupamiento. 
+*/
+
+-- CONSULTA DE AGRUPAMIENTO CONCONDICIÓN
+SELECT COUNT(titulo) AS 'NUMERO DE ENTRADAS', categoria_id FROM 
+entradas GROUP BY categoria_id HAVING COUNT(titulo) >=3;
