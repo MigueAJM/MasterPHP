cbf4052746ab46b06cd121353aaa34aee2c9dd24
MiGueAJM9724 <miguejm@outlook.es>
2020-10-08 09:01:20 -0500

    funciones de agrupamiento

--- master-php/aprendiendo-sql/05-agrupamiento/consultas.sql
+++ master-php/aprendiendo-sql/05-agrupamiento/consultas.sql
@@ -11,3 +11,19 @@ a agrupamiento.
 -- CONSULTA DE AGRUPAMIENTO CONCONDICIÓN
 SELECT COUNT(titulo) AS 'NUMERO DE ENTRADAS', categoria_id FROM 
 entradas GROUP BY categoria_id HAVING COUNT(titulo) >=3;
+
+-- FUNCONES DE AGRUPAMIENTO
+/*
+    COUNT   CONTAR NUMERO DE ELEMENTOS
+    AVG     MEDIA
+    MAX     MAXIMO
+    MIN     MINIMO
+    SUM     SUMAR TODO EL CONTENIDO DEL GRUPO
+*/
+SELECT * FROM entradas;
+SELECT AVG(id) AS 'MEDIA DE ENTRADAS' FROM entradas;
+
+SELECT MAX(id) AS 'Maximo id', titulo FROM entradas;  
+SELECT MIN(id) AS 'Minimo id', titulo FROM entradas;
+
+SELECT SUM(id) AS 'Suma de id' FROM entradas;
