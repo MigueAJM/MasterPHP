ab53b48689f81199a42ff2be626f21e3be586b53
MiGueAJM9724 <miguejm@outlook.es>
2020-10-10 09:07:08 -0500

    ejemplos coonsultas multitabla

--- master-php/aprendiendo-sql/07-multitablas/consultas.sql
+++ master-php/aprendiendo-sql/07-multitablas/consultas.sql
@@ -5,3 +5,11 @@ Son cunsultas que sirven para consultar vsrias tablas en una sola sentencia
 SELECT e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoria' 
 FROM entradas e, usuarios u, categorias c
 WHERE e.usuario_id = u.id AND e.categoria_id = c.id;
+
+-- MOSTRAR EK NOMBRRE DE LAS CATEGORIAS Y AL LADO CUENTAS ENTRADAS TIENEN
+SELECT c.nombre, COUNT(e.id) AS 'Numero Entradas' FROM categorias c, entradas e
+WHERE e.categoria_id = c.id GROUP BY e.categoria_id;
+
+-- MSOTRAR EL EMAIL DE LOS USUARIOS Y AL LADO CUANTAS ENTRADAS TIENEN
+SELECT u.email, COUNT(titulo) AS 'Numero Entradas'FROM usuarios u, entradas e
+WHERE e.usuario_id = u.id GROUP BY e.usuario_id; 
