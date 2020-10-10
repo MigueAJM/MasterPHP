5817df13a0e5029d9664d1f8903ed3763419e281
MiGueAJM9724 <miguejm@outlook.es>
2020-10-10 09:19:36 -0500

    INNER JOIN

--- master-php/aprendiendo-sql/07-multitablas/consultas.sql
+++ master-php/aprendiendo-sql/07-multitablas/consultas.sql
@@ -13,3 +13,16 @@ WHERE e.categoria_id = c.id GROUP BY e.categoria_id;
 -- MSOTRAR EL EMAIL DE LOS USUARIOS Y AL LADO CUANTAS ENTRADAS TIENEN
 SELECT u.email, COUNT(titulo) AS 'Numero Entradas'FROM usuarios u, entradas e
 WHERE e.usuario_id = u.id GROUP BY e.usuario_id; 
+
+
+/*
+INNER JOIN 
+El INNER JOIN se diferencia de todas las demás sentencias de JOIN por mostrar 
+un conjunto de resultados mínimos, pues solo se muestran los registros de datos 
+del producto cruzado que cumplen la condición de selección. Todo ello se presenta 
+en una tabla de resultados llamada “View” que no incluye valores nulos.
+*/
+SELECT e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoria' 
+FROM entradas e
+INNER JOIN usuarios u ON e.usuario_id = u.id
+INNER JOIN categorias c  ON e.categoria_id = c.id; 
