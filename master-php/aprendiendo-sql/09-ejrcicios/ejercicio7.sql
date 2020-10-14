8609f923b0c2ec9ab0540df91b7bad96f31c0bb0
MiGueAJM9724 <miguejm@outlook.es>
2020-10-13 19:55:21 -0500

    ejercicio7 SQL

---
+++ master-php/aprendiendo-sql/09-ejrcicios/ejercicio7.sql
@@ -0,0 +1,5 @@
+/*
+MOSTRAR EL NOMBRE Y EL SALARIO DE LOS VENDEDORES CON CARGO
+DE 'Ayudante en la tienda' 
+*/
+SELECT nombre, sueldo AS 'Salario' FROM vendedores WHERE cargo = 'Ayudante en la tienda';
