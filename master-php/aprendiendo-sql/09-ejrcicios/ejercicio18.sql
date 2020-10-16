ceff30242a74548c611d4ddd134e980614c4dde2
MiGueAJM9724 <miguejm@outlook.es>
2020-10-16 12:31:08 -0500

    ejercicio18 SQL

---
+++ master-php/aprendiendo-sql/09-ejrcicios/ejercicio18.sql
@@ -0,0 +1,7 @@
+/*
+OBTENER UN LISTADO DE CLIENTES QUE HAN HECHO UN ENCARGO DEL COCHE
+'Porche Cayene'
+*/
+SELECT * FROM clientes WHERE id IN
+(SELECT cliente_id FROM encargos WHERE coche_id 
+    IN (SELECT id FROM coches WHERE modelo LIKE 'Porche Cayene'));
