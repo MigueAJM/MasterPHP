0789cc09749568fd39f36e93888f1e3dbb8e4b0f
MiGueAJM9724 <miguejm@outlook.es>
2020-10-17 18:16:57 -0500

    HTML menu de navegación

--- master-php/proyecyo-php/assets/index.php
+++ master-php/proyecyo-php/assets/index.php
@@ -6,14 +6,42 @@
     </head>
     <body>
         <!--CABECERA-->
-        <header>
+        <header id="header">
+            <!--LOGO-->
             <div id="logo">
                 <a href="index.php">
                     Blog de videojuegos
                 </a>
             </div>
-        </header>/
-        <!--MENU-->
+
+            <!--MENU-->
+            <nav id="nav">
+                <ul>
+                    <li>
+                        <a href="index.php">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 3</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 4</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Sobre mi</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        
 
         <!--BARRA LATERAL-->
 
