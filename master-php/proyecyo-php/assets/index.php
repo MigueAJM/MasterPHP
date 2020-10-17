798720fe7a14ca79c7070dbca054d302a7ea6722
MiGueAJM9724 <miguejm@outlook.es>
2020-10-17 18:41:03 -0500

    HTML Contenido principal

--- master-php/proyecyo-php/assets/index.php
+++ master-php/proyecyo-php/assets/index.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <!--CABECERA-->
-        <header id="header">
+        <header id="cabecera">
             <!--LOGO-->
             <div id="logo">
                 <a href="index.php">
@@ -15,7 +15,7 @@
             </div>
 
             <!--MENU-->
-            <nav id="nav">
+            <nav id="menu">
                 <ul>
                     <li>
                         <a href="index.php">Inicio</a>
@@ -42,11 +42,82 @@
             </nav>
         </header>
         
+        <div id="contenedor">
+                    <!--BARRA LATERAL-->
+            <aside id="sidebar">
+                <div id="login" class="bloque">
+                    <h3>Identificate</h3>
+                    <form action="login.php" method="POST">
+                        <label for="email">Emial</label>
+                        <input type="email" name="email">
 
-        <!--BARRA LATERAL-->
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password">
 
-        <!--CAJA PRINCIPAL-->
+                        <input type="submit" value="Entrar">
+                    </form>   
+                </div>
+                <div id="register" class="bloque">
+                    <h3>Identificate</h3>
+                    <form action="registro.php" method="POST">
+                        <label for="nombre">Emial</label>
+                        <input type="text" name="nombre">
 
+                        <label for="apellidos">Emial</label>
+                        <input type="text" name="apellidos">
+
+                        <label for="email">Emial</label>
+                        <input type="email" name="email">
+
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password">
+
+                        <input type="submit" value="Registrar">
+                    </form>   
+                </div>
+            </aside>
+        </div>
+        <div id="principal">
+            <!--CAJA PRINCIPAL-->
+            <h1>Ultimas entradas</h1>
+
+            <article class="entada">
+                <h2>Titulo de mi entradas</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Modi harum animi molestias molestiae consequatur voluptatem, 
+                    nesciunt quisquam deleniti nulla a id perferendis ratione, 
+                    deserunt excepturi. Natus sed veritatis accusantium numquam.
+                </p>
+            </article>
+            <article class="entada">
+                <h2>Titulo de mi entradas</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Modi harum animi molestias molestiae consequatur voluptatem, 
+                    nesciunt quisquam deleniti nulla a id perferendis ratione, 
+                    deserunt excepturi. Natus sed veritatis accusantium numquam.
+                </p>
+            </article>
+            <article class="entada">
+                <h2>Titulo de mi entradas</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Modi harum animi molestias molestiae consequatur voluptatem, 
+                    nesciunt quisquam deleniti nulla a id perferendis ratione, 
+                    deserunt excepturi. Natus sed veritatis accusantium numquam.
+                </p>
+            </article>
+            <article class="entada">
+                <h2>Titulo de mi entradas</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Modi harum animi molestias molestiae consequatur voluptatem, 
+                    nesciunt quisquam deleniti nulla a id perferendis ratione, 
+                    deserunt excepturi. Natus sed veritatis accusantium numquam.
+                </p>
+            </article>
+        </div>
         <!--PIE DE PAGINA-->
 
 
