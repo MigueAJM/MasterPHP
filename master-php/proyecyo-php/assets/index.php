4c90dcbe25f9404f67984172cf1cb2a8c43e6af1
MiGueAJM9724 <miguejm@outlook.es>
2020-10-17 18:43:32 -0500

    HTML pie de pagina

--- master-php/proyecyo-php/assets/index.php
+++ master-php/proyecyo-php/assets/index.php
@@ -118,6 +118,10 @@
                 </p>
             </article>
         </div>
+
+        <footer id="pie">
+            <p>Desarrollado por Miguel Jimenez &copy; 2020</p>
+        </footer>
         <!--PIE DE PAGINA-->
 
 
