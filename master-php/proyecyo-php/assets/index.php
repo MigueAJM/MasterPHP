31ca0171d8db5fa84b7732bb276ee47b9fa24836
MiGueAJM9724 <miguejm@outlook.es>
2020-10-17 17:59:18 -0500

    HTML cabecera

--- master-php/proyecyo-php/assets/index.php
+++ master-php/proyecyo-php/assets/index.php
@@ -1,1 +1,26 @@
-<h1>HOLA MUNOO</h1>
+<! DOCTYPE HTML>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Blog de videojuegos</title>
+    </head>
+    <body>
+        <!--CABECERA-->
+        <header>
+            <div id="logo">
+                <a href="index.php">
+                    Blog de videojuegos
+                </a>
+            </div>
+        </header>/
+        <!--MENU-->
+
+        <!--BARRA LATERAL-->
+
+        <!--CAJA PRINCIPAL-->
+
+        <!--PIE DE PAGINA-->
+
+
+    </body>
+</html>
