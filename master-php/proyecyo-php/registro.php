3e7bbf06503cf3a5740df6e5b567915860988865
MiGueAJM9724 <miguejm@outlook.es>
2020-10-20 12:27:26 -0500

    194. Registro de usuarios(Validando_input

--- master-php/proyecyo-php/registro.php
+++ master-php/proyecyo-php/registro.php
@@ -18,7 +18,8 @@ NOTA:   OPERADORES TERNARIOS
         $horario_de_apertura = 8;
     }
 */
-if(isset($_POST['submit'])){
+if(isset($_POST)){
+    /*RECOLECCIÓN DE LA INFORMACIÓN DEL FORMULARIO*/
     //  condicional if
     if(isset($_POST['nombre'])){
         $name = $_POST['nombre'];
@@ -26,8 +27,47 @@ if(isset($_POST['submit'])){
         $name = false;
     }
     //  operador ternario
-     $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
-     $email = isset($_POST['email']) ? $_POST['email'] : false;
-     $password = isset($_POST['password']) ? $_POST['password'] : false;
-    var_dump($_POST);
+    $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
+    $email = isset($_POST['email']) ? $_POST['email'] : false;
+    $password = isset($_POST['password']) ? $_POST['password'] : false;
+    
+    //  array de errores
+    $errores = array();
+
+    // validar los datos antes de almacenar en la base de datos
+    if(!empty($name) && !is_numeric($name) && !preg_match("/[0-9]/", $name)){
+        $validate_name = true;
+    } else{
+        $validate_name = false;
+        $errores['nombre'] = 'El nombre no es valido';
+    }
+
+    if(!empty($apellidos) && !is_numeric($apellidos) && !preg_match("/[0-9]/", $apellidos)){
+        $validate_apellidos= true;
+    } else{
+        $validate_apellidos = false;
+        $errores['apellidos'] = 'los apellidos no son validos';
+    }
+
+    if(!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $validate_email = true;
+    } else{
+        $validate_email = false;
+        $errores['email'] = 'El email no es valido';
+    }
+
+    if(!empty($password)){
+        $validate_password = true;
+    } else{
+        $validate_password = false;
+        $errores['password'] = 'La contraseña esta vacia';
+    }
+
+    $save_user = false;
+    if(count($errores) == 0){
+        //  INSERTAR USUARIO EN LA BASE DE DATOS EN LA TABLA DE USUARIOS
+        $save_user =  true;
+    }else{
+
+    }
 }
